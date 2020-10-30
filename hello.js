2acec60bfb79ed0b10f9b7dac9e7ba95508673a8
sahanakumar36@gmail.com <71411572+sahanakumar36@users.noreply.github.com>
2020-10-30 13:43:24 -0400

    Added JSON file to original code

---
+++ hello.js
@@ -0,0 +1,39 @@
+/* 
+URL Module Query String
+var http = require('http');
+var url = require('url');
+
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var q = url.parse(req.url, true).query;
+    var dates = q.weekday + ", " + q.month + " " + q.day + ", " + q.year;
+    res.end(dates);
+}).listen(8080); */
+
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+
+http
+	.createServer(function (req, res) {
+		var q = url.parse(req.url, true);
+		var filename = "." + q.pathname;
+		if (filename == "./") {
+			filename = "./index";
+		}
+
+		filename = filename + ".html";
+		fs.readFile(filename, function (err, data) {
+			if (err) {
+				res.writeHead(404, { "Content-Type": "text/html" });
+				return res.end("404 Not Found");
+			}
+			res.writeHead(200, { "Content-Type": "text/html" });
+			res.write(data);
+			return res.end();
+		});
+	})
+	.listen(8080);
+
+console.log("Listening on Port 8080...");
